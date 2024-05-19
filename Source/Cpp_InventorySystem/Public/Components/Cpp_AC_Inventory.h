da8e0901c378cc522b59e8f66d16d671ab5ba314
TanushKrashak <tankrashak38816@gmail.com>
2024-05-19 15:03:44 +0530

    Enum & Struct For AC_Inventory

--- Source/Cpp_InventorySystem/Public/Components/Cpp_AC_Inventory.h
+++ Source/Cpp_InventorySystem/Public/Components/Cpp_AC_Inventory.h
@@ -6,6 +6,57 @@
 #include "Components/ActorComponent.h"
 #include "Cpp_AC_Inventory.generated.h"
 
+UENUM(BlueprintType)
+enum class EItemAddResult : uint8 {
+	IAR_NoItemsAdded UMETA(DisplayName = "No Items Added"),
+	IAR_SomeItemsAdded UMETA(DisplayName = "Some Items Added"),
+	IAR_AllItemsAdded UMETA(DisplayName = "All Items Added")
+};
+
+USTRUCT(BlueprintType)
+struct FItemAddResult {
+	GENERATED_BODY()
+
+	FItemAddResult() :
+		ActualAmountAdded(0),
+		OperationResult(EItemAddResult::IAR_NoItemsAdded),
+		ResultMessage(FText::GetEmpty())
+	{};
+
+	// Actual amount of items that was added to the inventory
+	UPROPERTY(BlueprintReadOnly, Category = "Item Add Result")
+	int32 ActualAmountAdded;
+	// Enum value indicating the result of the add item operation
+	UPROPERTY(BlueprintReadOnly, Category = "Item Add Result")
+	EItemAddResult OperationResult;
+	// Informational message about the result of the add item operation
+	UPROPERTY(BlueprintReadOnly, Category = "Item Add Result")
+	FText ResultMessage;
+
+	// Static methods as they are not dependent on the state of the object
+	static FItemAddResult AddedNone(const FText& ErrorText) {
+		FItemAddResult AddedNoneResult;
+		AddedNoneResult.ActualAmountAdded = 0;
+		AddedNoneResult.OperationResult = EItemAddResult::IAR_NoItemsAdded;
+		AddedNoneResult.ResultMessage = ErrorText;
+		return AddedNoneResult;
+	}
+	static FItemAddResult AddedSome(const int32 SomeAmountAdded, const FText& ErrorText) {
+		FItemAddResult AddedSomeResult;
+		AddedSomeResult.ActualAmountAdded = SomeAmountAdded;
+		AddedSomeResult.OperationResult = EItemAddResult::IAR_SomeItemsAdded;
+		AddedSomeResult.ResultMessage = ErrorText;
+		return AddedSomeResult;
+	}
+	static FItemAddResult AddedAll(const int32 AmountAdded, const FText& Message) {
+		FItemAddResult AddedAllResult;
+		AddedAllResult.ActualAmountAdded = AmountAdded;
+		AddedAllResult.OperationResult = EItemAddResult::IAR_AllItemsAdded;
+		AddedAllResult.ResultMessage = Message;
+		return AddedAllResult;
+	}
+
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CPP_INVENTORYSYSTEM_API UCpp_AC_Inventory : public UActorComponent
@@ -23,8 +74,7 @@ public:
 	// FUNCTIONS
 	//====================================================================================================================
 
-	UCpp_AC_Inventory();
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UCpp_AC_Inventory();	
 
 		
 	
@@ -41,4 +91,5 @@ protected:
 	//====================================================================================================================
 	
 	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
