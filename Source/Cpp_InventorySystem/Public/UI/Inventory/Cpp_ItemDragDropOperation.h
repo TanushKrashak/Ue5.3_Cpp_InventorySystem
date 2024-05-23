84f8d9306686755ffc7a5c900cdda5ddd8b2ff04
TanushKrashak <tankrashak38816@gmail.com>
2024-05-23 18:17:26 +0530

    Created Widgets + DragDropOperation
    Created Cpp_WGT_InventoryToolTip
    Created Cpp_WGT_DragItem
    Created Cpp_ItemDragDropOperation

---
+++ Source/Cpp_InventorySystem/Public/UI/Inventory/Cpp_ItemDragDropOperation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/DragDropOperation.h"
+#include "Cpp_ItemDragDropOperation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_INVENTORYSYSTEM_API UCpp_ItemDragDropOperation : public UDragDropOperation
+{
+	GENERATED_BODY()
+	
+};
