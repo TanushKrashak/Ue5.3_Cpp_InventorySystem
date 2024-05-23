c537730acd98d0b973c71cedf08d2024811a5c05
TanushKrashak <tankrashak38816@gmail.com>
2024-05-23 19:18:58 +0530

    Implem of RefreshInventory() & SetInfoText() in Cpp_WGT_InventoryPanel

--- Source/Cpp_InventorySystem/Private/UI/Inventory/Cpp_WGT_InventoryPanel.cpp
+++ Source/Cpp_InventorySystem/Private/UI/Inventory/Cpp_WGT_InventoryPanel.cpp
@@ -1,9 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+// Game
 #include "UI/Inventory/Cpp_WGT_InventoryPanel.h"
 #include "../Cpp_InventorySystemCharacter.h"
+#include "UI/Inventory/Cpp_WGT_InventoryItemSlot.h
+#include "Components/Cpp_AC_Inventory.h"
+
+// Engine
 #include "Components/TextBlock.h"
+#include "Components/WrapBox.h"
+
+
+
 
 void UCpp_WGT_InventoryPanel::NativeOnInitialized() {
 	Super::NativeOnInitialized();
@@ -25,7 +33,18 @@ bool UCpp_WGT_InventoryPanel::NativeOnDrop(const FGeometry& InGeometry, const FD
 }
 
 void UCpp_WGT_InventoryPanel::RefreshInventory() {
+	if (InventoryReference && InventoryItemSlotClass) {
+		WB_InventoryPanel->ClearChildren();
+		
+		// Iterate Through Inventory
+		for (UItemBase* const& InventoryItem : InventoryReference->GetInventoryContents()) {
+			UCpp_WGT_InventoryItemSlot* ItemSlot = CreateWidget<UCpp_WGT_InventoryItemSlot>(this, InventoryItemSlotClass);
+			//ItemSlot->SetItemReference(InventoryItem);
 
+			WB_InventoryPanel->AddChildToWrapBox(ItemSlot);
+		}
+
+	}
 }
 
 void UCpp_WGT_InventoryPanel::SetInfoText() const {
