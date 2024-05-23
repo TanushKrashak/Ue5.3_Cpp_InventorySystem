5289066e4a7d539553eff0a860f54c37759d34e0
TanushKrashak <tankrashak38816@gmail.com>
2024-05-23 18:57:41 +0530

    Creation + Implementation in Cpp_WGT_InventoryPanel
    Created Widget Info Variables
    Created Binding Function RefreshInventory()
    Created Function SetInfoText() called on Init
    Implem of NativeOnInit & NativeOnDrop

--- Source/Cpp_InventorySystem/Private/UI/Inventory/Cpp_WGT_InventoryPanel.cpp
+++ Source/Cpp_InventorySystem/Private/UI/Inventory/Cpp_WGT_InventoryPanel.cpp
@@ -2,4 +2,39 @@
 
 
 #include "UI/Inventory/Cpp_WGT_InventoryPanel.h"
+#include "../Cpp_InventorySystemCharacter.h"
+#include "Components/TextBlock.h"
+
+void UCpp_WGT_InventoryPanel::NativeOnInitialized() {
+	Super::NativeOnInitialized();
+
+	PlayerCharacter = Cast<ACpp_InventorySystemCharacter>(GetOwningPlayerPawn());
+
+	if (PlayerCharacter) {
+		InventoryReference = PlayerCharacter->GetInventory();
+		if (InventoryReference) {
+			InventoryReference->OnInventoryUpdated.AddUObject(this, &UCpp_WGT_InventoryPanel::RefreshInventory);
+			SetInfoText();
+		}
+	}
+}
+
+bool UCpp_WGT_InventoryPanel::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) {
+	Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
+
+}
+
+void UCpp_WGT_InventoryPanel::RefreshInventory() {
+
+}
+
+void UCpp_WGT_InventoryPanel::SetInfoText() const {
+	TB_WeightInfo->SetText(FText::Format(FText::FromString("{0}/{1}"), 
+										 InventoryReference->GetInventoryTotalWeight(),
+										 InventoryReference->GetWeightCapacity()));
+	TB_CapacityInfo->SetText(FText::Format(FText::FromString("{0}/{1}"),
+										 InventoryReference->GetInventoryContents().Num(),
+										 InventoryReference->GetSlotsCapacity()));
+}
+
 
--- Source/Cpp_InventorySystem/Public/UI/Inventory/Cpp_WGT_InventoryPanel.h
+++ Source/Cpp_InventorySystem/Public/UI/Inventory/Cpp_WGT_InventoryPanel.h
@@ -6,12 +6,45 @@
 #include "Blueprint/UserWidget.h"
 #include "Cpp_WGT_InventoryPanel.generated.h"
 
+class ACpp_InventorySystemCharacter;
+class UCpp_AC_Inventory;
+class Cpp_WGT_InventoryItemSlot;
+
 /**
  * 
  */
 UCLASS()
 class CPP_INVENTORYSYSTEM_API UCpp_WGT_InventoryPanel : public UUserWidget
 {
-	GENERATED_BODY()
+	GENERATED_BODY()	
 	
+public:
+	UFUNCTION()
+	void RefreshInventory();
+
+	UPROPERTY(meta = (BindWidget))
+	UWrapBox* WB_InventoryPanel;
+	
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TB_WeightInfo;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TB_CapacityInfo;
+
+	UPROPERTY()
+	ACpp_InventorySystemCharacter* PlayerCharacter;
+
+	UPROPERTY()
+	UCpp_AC_Inventory* InventoryReference;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UCpp_WGT_InventoryItemSlot> InventoryItemSlotClass;
+
+
+protected:
+
+	virtual void NativeOnInitialized() override;
+	virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+
+	void SetInfoText() const;
 };
