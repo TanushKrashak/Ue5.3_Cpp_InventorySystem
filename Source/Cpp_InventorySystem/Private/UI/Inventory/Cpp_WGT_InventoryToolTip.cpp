9954dc07c54457dbc048178aa243e4d8e90eb14a
TanushKrashak <tankrashak38816@gmail.com>
2024-05-25 20:26:17 +0530

    Update: Cpp_WGT_InventoryToolTip.cpp: Implem of NativeConstruct()
    Sets values of text variables based on ItemType

--- Source/Cpp_InventorySystem/Private/UI/Inventory/Cpp_WGT_InventoryToolTip.cpp
+++ Source/Cpp_InventorySystem/Private/UI/Inventory/Cpp_WGT_InventoryToolTip.cpp
@@ -2,4 +2,53 @@
 
 
 #include "UI/Inventory/Cpp_WGT_InventoryToolTip.h"
+#include "UI/Inventory/Cpp_WGT_InventoryItemSlot.h"
+#include "ItemBase.h"
+#include "Components/TextBlock.h"
 
+void UCpp_WGT_InventoryToolTip::NativeConstruct() {
+	Super::NativeConstruct();
+
+	const UItemBase* ItemBeingHovered = InventorySlotBeingHovered->GetItemReference();
+
+	switch (ItemBeingHovered->ItemType) {
+		case EItemType::Weapon:
+			break;
+		case EItemType::Armor:
+			break;
+		case EItemType::Consumable:
+			TXT_ItemType->SetText(FText::FromString("Consumable"));
+			TXT_DamageValue->SetVisibility(ESlateVisibility::Collapsed);
+			TXT_ArmorRating->SetVisibility(ESlateVisibility::Collapsed);
+			TXT_SellValue->Visibility(ESlateVisibility::Collapsed);
+			break;
+		case EItemType::Spell:
+			break;
+		case EItemType::Quest:
+			break;
+		case EItemType::Other:
+			TXT_ItemType->SetText(FText::FromString("Miscellaneous Item"));
+			TXT_DamageValue->SetVisibility(ESlateVisibility::Collapsed);
+			TXT_ArmorRating->SetVisibility(ESlateVisibility::Collapsed);
+			TXT_Usage->SetVisibility(ESlateVisibility::Collapsed);
+			TXT_SellValue->Visibility(ESlateVisibility::Collapsed);
+			break;
+		default:
+			break;
+	}
+
+	TXT_ItemName->SetText(FText::FromString(ItemBeingHovered->ItemTextData.ItemName));
+	TXT_DamageValue->SetText(FText::AsNumber(ItemBeingHovered->ItemStatistics.DamageValue));
+	TXT_ArmorRating->SetText(FText::AsNumber(ItemBeingHovered->ItemStatistics.ArmorRating));
+	TXT_Usage->SetText(FText::FromString(ItemBeingHovered->ItemTextData.UsageText));
+	TXT_ItemDescription->SetText(FText::FromString(ItemBeingHovered->ItemTextData.ItemDescription));
+	TXT_SellValue->SetText(FText::AsNumber(ItemBeingHovered->ItemStatistics.SellValue));
+	TXT_StackWeight->SetText(FText::AsNumber(ItemBeingHovered->GetItemStackWeight());
+
+	if (ItemBeingHovered->ItemNumericData.bIsStackable) {
+		TXT_MaxStackSize->SetText(FText::AsNumber(ItemBeingHovered->ItemNumericData.MaxStackSize));
+	}
+	else {
+		TXT_MaxStackSize->SetVisibility(ESlateVisibility::Collapsed);
+	}
+}
