968c4a373b525027a2bb9328c4aaedb0fc248533
TanushKrashak <tankrashak38816@gmail.com>
2024-05-25 20:25:18 +0530

    Update Cpp_WGT_InventoryToolTip.h: Textblock Variables + NativeConstruct()

--- Source/Cpp_InventorySystem/Public/UI/Inventory/Cpp_WGT_InventoryToolTip.h
+++ Source/Cpp_InventorySystem/Public/UI/Inventory/Cpp_WGT_InventoryToolTip.h
@@ -6,6 +6,9 @@
 #include "Blueprint/UserWidget.h"
 #include "Cpp_WGT_InventoryToolTip.generated.h"
 
+class UCpp_WGT_InventoryItemSlot;
+class UTextBlock;
+
 /**
  * 
  */
@@ -14,4 +17,38 @@ class CPP_INVENTORYSYSTEM_API UCpp_WGT_InventoryToolTip : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(VisibleAnywhere)
+	UCpp_WGT_InventoryItemSlot* InventorySlotBeingHovered;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TXT_ItemName;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TXT_ItemType;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TXT_DamageValue;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TXT_ArmorRating;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TXT_Usage;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TXT_ItemDescription;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TXT_StackSize;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TXT_SellValue;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TXT_StackWeight;
+
+protected:
+
+	virtual void NativeConstruct() override;
 };
