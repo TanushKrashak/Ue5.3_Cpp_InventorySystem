df82552064eb7d7f45acbf60eadb316e90e75d0a
TanushKrashak <tankrashak38816@gmail.com>
2024-05-25 20:01:56 +0530

    Upt: Cpp_WGT_InventoryItemSlot.cpp Implementation of NativeOnInit() & NativeConstruct()

--- Source/Cpp_InventorySystem/Private/UI/Inventory/Cpp_WGT_InventoryItemSlot.cpp
+++ Source/Cpp_InventorySystem/Private/UI/Inventory/Cpp_WGT_InventoryItemSlot.cpp
@@ -2,6 +2,11 @@
 
 
 #include "UI/Inventory/Cpp_WGT_InventoryItemSlot.h"
+#include "UI/Inventory/Cpp_ItemDragDropOperation.h"
+#include "UI/Inventory/Cpp_WGT_InventoryToolTip.h"
+#include "Components/Border.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
 
 const UE::FieldNotification::IClassDescriptor& UCpp_WGT_InventoryItemSlot::GetFieldNotificationDescriptor() const {
 	
@@ -10,11 +15,47 @@ const UE::FieldNotification::IClassDescriptor& UCpp_WGT_InventoryItemSlot::GetFi
 
 void UCpp_WGT_InventoryItemSlot::NativeOnInitialized() {
 	Super::NativeOnInitialized();
+	
+	if (ToolTipClass) {
+		UCpp_WGT_InventoryToolTip* ToolTip = CreateWidget<UCpp_WGT_InventoryToolTip>(this, ToolTipClass);
+		// ToolTip->InventorySlotBeingHovered = this;
+		SetToolTip(ToolTip);
+	}
 
 }
+
 void UCpp_WGT_InventoryItemSlot::NativeConstruct() {
 	Super::NativeConstruct();
 	
+	if (ItemReference) {
+		switch (ItemReference->ItemQuality) {
+			case EItemQuality::Common:
+				Border_Item->SetBrushColor(FLinearColor::Gray);
+				break;
+			case EItemQuality::Uncommon:
+				Border_Item->SetBrushColor(FLinearColor::Green);
+				break;
+			case EItemQuality::Rare:
+				Border_Item->SetBrushColor(FLinearColor::Blue);
+				break;
+			case EItemQuality::Epic:
+				Border_Item->SetBrushColor(FLinearColor::Purple);
+				break;
+			case EItemQuality::Legendary:
+				Border_Item->SetBrushColor(FLinearColor(100.0f, 65.0f, 0.0f, 1.0f)); // Solid Orange Color
+				break;		
+			default: 
+				break;
+		}	
+
+		IMG_Icon->SetBrushFromTexture(ItemReference->ItemAssetData.Icon);
+		if (ItemReference->ItemNumericData.bIsStackable) {
+			TXT_Quantity->SetText(FText::AsNumber(ItemReference->Quantity));
+		}
+		else {
+			TXT_Quantity->SetVisibility(ESlateVisibility::Collapsed);
+		}
+	}
 }
 void UCpp_WGT_InventoryItemSlot::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation) {
 	Super::NativeOnDragDetected(InGeometry, InMouseEvent, OutOperation);
